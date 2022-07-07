f604b6300531949f87aa45e79c9f7cad060281b6
tony7kh <antonkhizhuk1981@gmail.com>
2022-07-07 17:01:11 +0300

    rick-and-morty

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+
+import data from "./films.json";
+import dataPersons from "./persons.json";
+
+const dataOfCharacters = dataPersons.data.characters.results;
+
+const Character = ({ person: { name, gender, image, episode } }) => (
+  <div class="person">
+    <img src={image} />
+    <div class="info">
+      <h2 class="name">{name}</h2>
+      <p class="gender">{gender}</p>
+      <ul class="listOfEpisodes">
+        {" "}
+        {episode.map((episode) => (
+          <li key={episode.id}>
+            {episode.name}
+            {episode.air_date}
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+);
+
+const Characters = ({ persons = dataOfCharacters }) => (
+  <div class="persons">
+    {persons.map((person) => (
+      <Character key={person.id} person={person} />
+    ))}
+  </div>
+);
+
+const dataOfProducts = data.data.episodes.results;
+
+const Person = ({ character: { id, image, name } }) => {
+  return (
+    <>
+      <div class="person" key={id}>
+        {name}
+        <img src={image} />
+      </div>
+    </>
+  );
+};
+const Episode = ({ product: { id, name, date, characters } }) => (
+  <div className="episode">
+    <h1>{name}</h1>
+    <strong>{date}</strong>
+    <div class="persons">
+      {characters.map((character) => (
+        <Person key={id} character={character}></Person>
+      ))}
+    </div>
+  </div>
+);
+
+const Episodes = ({ products = dataOfProducts }) => (
+  <div className="Episodes">
+    {products.map((product) => (
+      <Episode key={product.id} product={product} />
+    ))}
+  </div>
+);
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Characters />
+      <Episodes />
+    </>
   );
 }
 
